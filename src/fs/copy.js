1f2a1ba2f27711a5c65307c412c21dae9511e4e2
Svitlana <svitlana.grytsai@gmail.com>
2024-10-02 22:09:47 +0200

    feat: implement copy function

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,13 @@
-const copy = async () => {
-    // Write your code here 
-};
+import fsPromises from "node:fs/promises";
 
+const copy = async () => {
+  try {
+    const src = "src/fs/files/";
+    const dest = "src/fs/files_copy/";
+    fsPromises.cp(src, dest, { recursive: true });
+  } catch (error) {
+    const errorMessage = "FS operation failed";
+    throw Error(errorMessage);
+  }
+};
 await copy();
