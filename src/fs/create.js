314bec8eec440686b3c1b11f00f7f096b7a3d7e6
Svitlana <svitlana.grytsai@gmail.com>
2024-10-02 21:46:00 +0200

    feat: implement create function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,14 @@
-const create = async () => {
-    // Write your code here 
-};
+import fsPromises from "node:fs/promises";
 
-await create();
+const create = async () => {
+  try {
+    const pathToSave = "src/fs/files/";
+    const fileName = "fresh.txt";
+    const fileContent = "I am fresh and young";
+    await fsPromises.writeFile(pathToSave + fileName, fileContent);
+  } catch (error) {
+    const errorMessage = "FS operation failed";
+    throw Error(errorMessage);
+  }
+};
+create();
