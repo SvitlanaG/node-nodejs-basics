0379c02b98cb977fe2ad11d7cf9cb6dbc594ea20
Svitlana <svitlana.grytsai@gmail.com>
2024-10-03 12:17:47 +0200

    feat: implement delete function

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,14 @@
+import fsPromises from "node:fs/promises";
+
 const remove = async () => {
-    // Write your code here 
+  try {
+    const pathToFile = "src/fs/files/";
+    const fileName = "fileToRemove.txt";
+    await fsPromises.rm(pathToFile + fileName);
+  } catch (error) {
+    const errorMessage = "FS operation failed";
+    throw Error(errorMessage);
+  }
 };
 
 await remove();
