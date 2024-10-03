83dc9c7c574ceaff43d9d2f6ad73570f3590347f
Svitlana <svitlana.grytsai@gmail.com>
2024-10-03 12:34:04 +0200

    feat: implement list function

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,15 @@
+import fsPromises from "node:fs/promises";
+
 const list = async () => {
-    // Write your code here 
+  try {
+    const path = "src/fs/files/";
+    const files = await fsPromises.readdir(path, { recursive: true });
+    for (const file of files) console.log(file);
+  } catch (error) {
+    console.error(error);
+    const errorMessage = "FS operation failed";
+    throw Error(errorMessage);
+  }
 };
 
 await list();
