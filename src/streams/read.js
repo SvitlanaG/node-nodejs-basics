cc09cac4f7c010df419ff3bfb0c073912ec6bdff
Svitlana <svitlana.grytsai@gmail.com>
2024-10-05 15:14:07 +0200

    feat: implement streams read, write and transform functions

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,24 @@
+import fs from "fs";
+import { throwError } from "../utils/customError.js";
+
 const read = async () => {
-    // Write your code here 
+  let data = "";
+  const pathToFile = "src/streams/files/";
+  const fileName = "fileToRead.txt";
+  const readerStream = fs.createReadStream(pathToFile + fileName);
+  readerStream.setEncoding("UTF8");
+
+  readerStream.on("data", function (chunk) {
+    data += chunk;
+  });
+
+  readerStream.on("end", function () {
+    console.log(data);
+  });
+
+  readerStream.on("error", function () {
+    throwError();
+  });
 };
 
 await read();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,18 @@
+import fs from "fs";
+import { throwError } from "../utils/customError.js";
+
 const write = async () => {
-    // Write your code here 
+  const pathToFile = "src/streams/files/";
+  const fileName = "fileToWrite.txt";
+  const writeStream = fs.createWriteStream(pathToFile + fileName);
+  process.stdin.pipe(writeStream);
+  process.stdin.resume();
+  writeStream.on("finish", () => {
+    console.log("Data successfully written to the file.");
+  });
+  writeStream.on("error", () => {
+    throwError();
+  });
 };
 
 await write();
