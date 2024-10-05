84d91461e6c61431580cc36215d2dd1af769b6fa
Svitlana <svitlana.grytsai@gmail.com>
2024-10-05 22:46:26 +0200

    feat: implement compress and decompress functions

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,28 @@
+import zlib from "zlib";
+import fs from "fs";
+import { throwError } from "../utils/customError.js";
+
 const compress = async () => {
-    // Write your code here 
+  const gzip = zlib.createGzip();
+  const pathToFile = "src/zip/files/";
+  const fileNameOriginal = "fileToCompress.txt";
+  const fileNameArchive = "archive.gz";
+  const input = fs.createReadStream(pathToFile + fileNameOriginal);
+  const output = fs.createWriteStream(pathToFile + fileNameArchive);
+
+  input.pipe(gzip).pipe(output);
+
+  input.on("error", () => {
+    throwError();
+  });
+
+  output.on("error", () => {
+    throwError();
+  });
+
+  gzip.on("error", () => {
+    throwError();
+  });
 };
 
 await compress();
